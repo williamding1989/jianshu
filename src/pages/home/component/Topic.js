1a5ec5145da11f2221320d08564f65e45150fea8
williamding_web@163.com <williamding_web@163.com>
2020-03-06 01:51:36 +0800

    函数组件

--- src/pages/home/component/Topic.js
+++ src/pages/home/component/Topic.js
@@ -1,27 +1,24 @@
-import React , { Component } from 'react'
+import React  from 'react'
 import {
 	TopicWrapper,
 	TopicItem
 } from '../style'
 
-class Topic extends Component{
-	render (){
-		const { topicList } = this.props
-		return (
-			<TopicWrapper>		
-				{
-					topicList.map((val,index)=>{
-						return (
-							<TopicItem key={val.id}>
-								<img alt='topicimg' className='topic-pic' src={val.imgUrl}></img>
-								{val.content}
-							</TopicItem>	
-						)
-					})	
-				}
-			</TopicWrapper>
-		)
-	}
+const Topic = (props) =>{
+	return (
+		<TopicWrapper>	
+			{
+				props.topicList.map((val,index)=>{
+					return (
+						<TopicItem key={val.id} >
+							<img alt='topicimg' className='topic-pic' src={val.imgUrl}></img>
+							{val.content}
+						</TopicItem>	
+					)
+				})	
+			}
+		</TopicWrapper>
+	)
 }
 
 //组件数据的默认值
